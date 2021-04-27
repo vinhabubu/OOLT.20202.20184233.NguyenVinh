be6cb63571b0fb41240f894e295b3029fb47ec1f
vinhabubu <77477174+vinhabubu@users.noreply.github.com>
2021-04-27 23:09:15 +0700

    Update Disc.java

--- lab8/AimsProject/hust/soict/hedspi/aims/media/Disc.java
+++ lab8/AimsProject/hust/soict/hedspi/aims/media/Disc.java
@@ -2,17 +2,27 @@ package hust.soict.hedspi.aims.media;
 
 
 public class Disc extends Media{
-   protected int length;
+   protected static int length;
    protected String director;
+public Disc() {
+    super(ID, title, category, cost);
+    Disc.length = length;
+    this.director = director;
+}
 
 
+public Disc(int length, String director, int ID, String title, String category, float cost) {
+    super(ID, title, category, cost);
+    this.length = length;
+    this.director = director;
+}
 
-   
+
 public int getLength() {
     return length;
 }
 public void setLength(int length) {
-    this.length = length;
+    Disc.length = length;
 }
 public String getDirector() {
     return director;
@@ -26,11 +36,4 @@ public void setDirector(String director) {
         
 		}
 
-
-
-
-
-
-
-    
 
