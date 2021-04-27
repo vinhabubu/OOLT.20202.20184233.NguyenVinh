94a732395033187aeccc5d3c23394619a7b297da
vinhabubu <77477174+vinhabubu@users.noreply.github.com>
2021-04-27 23:08:42 +0700

    Update DigitalVideoDisc.java

--- lab8/AimsProject/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
+++ lab8/AimsProject/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
@@ -13,6 +13,13 @@ public class DigitalVideoDisc extends Disc implements Introable {
     	super();
     }
 
+    public DigitalVideoDisc(int length, String director, int ID, String title, String category, float cost,
+            String director2, int length2) {
+        super(length, director, ID, title, category, cost);
+        director = director2;
+        length = length2;
+    }
+
     public DigitalVideoDisc(String director,int length) 
     {
     	super();
@@ -42,20 +49,32 @@ public class DigitalVideoDisc extends Disc implements Introable {
     	String[] split_title = title.split("--------"); 
     	for(String a : split_title) 
         {
-    		if(!this.title.contains(a)) 
+    		if(!Media.title.contains(a)) 
     			return  false;
     	}
     	return true;
     }
 
-    
+    public int compareTo(Object obj) {
+        int cCheck = Float.valueOf(this.cost).compareTo(((DigitalVideoDisc)obj).getCost());
+        int lCheck = Integer.valueOf(this.length).compareTo(((DigitalVideoDisc)obj).getLength());
+        if(cCheck != 0) {
+        	return cCheck;
+        }
+        else if (lCheck == 1) {
+        	return lCheck;
+        }
+        else {
+        	return this.title.compareTo(((DigitalVideoDisc)obj).getTitle());
+        }
 
-    public DigitalVideoDisc(String title, String category, float cost,String director, int length) {
-		super();
-		this.director = director;
-		this.length = length;
-	}
-    public void intro2() {
+    // public DigitalVideoDisc(String title, String category, float cost,String director, int length) {
+	// 	super();
+	// 	this.director = director;
+	// 	this.length = length;
+	// }
+    public void show() {
+        System.out.println("DVD is " + this.getID());
 		System.out.println("Title video is " + this.getTitle());
 		System.out.println("DVD length is  " + this.getLength());
 		System.out.println("Cost of DVD is" + this.getCost());
@@ -67,6 +86,9 @@ public class DigitalVideoDisc extends Disc implements Introable {
     public void intro() {
         // TODO Auto-generated method stub
         
+    }
+
+    public void play() {
     }
 	
 
