e552d1a95d9ffbc90f52ed1406789a13a5c464ba
vinhabubu <77477174+vinhabubu@users.noreply.github.com>
2021-05-04 23:49:04 +0700

    Update CreateFrame.java

--- lab9/AimsProject/hust/soict/hedspi/gui/CreateFrame.java
+++ lab9/AimsProject/hust/soict/hedspi/gui/CreateFrame.java
@@ -1,11 +1,16 @@
+package hust.soict.hedspi.aims.gui;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package com.hocjava.JavaSwing;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 /**
  *
  * @author Admin
